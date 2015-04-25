b754cb82eff44cce3b9fdc341e5710079611b2ee
michal zagrodzki <michalzagrodzki@gmail.com>
2015-04-25 12:53:02 +0200

    Add view for Task index. Add controllers for tasks.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,24 +5,4 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
-  def index
-    @tasks = Task.all
-  end
-
-  def create
-    #current_user.authored_tasks
-    @task = current_user.task.new(task_params)
-    if @task.save
-      redirect_to tasks_path, success: "Task created #{@task.name}."
-    else
-      render :new, error: "Something gone wrong. Please try again."
-    end
-  end
-
-  private
-
-  def task_params
-    params.require(:task).permit(:name, :desc, :author_id, :owner_id, :status)
-  end
-
 end
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,26 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = current_user.authored_tasks.new
+  end
+
+  def create
+    @task = current_user.authored_tasks.new(task_params)
+    if @task.save
+      redirect_to tasks_path, success: "Task created #{@task.name}."
+    else
+      render :new, error: "Something gone wrong. Please try again."
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :desc, :author_id, :owner_id, :status)
+  end
+
+end
