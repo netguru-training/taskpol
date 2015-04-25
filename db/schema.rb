1896093690599edb26901f77e16de9e4cddfcd22
michal zagrodzki <michalzagrodzki@gmail.com>
2015-04-25 15:37:26 +0200

    Add comment to schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425115501) do
+ActiveRecord::Schema.define(version: 20150425132321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "desc"
+    t.integer  "author_id"
+  end
 
   create_table "project_users", force: true do |t|
     t.integer  "project_id"
