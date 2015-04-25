039bb3e12a80a715cefe115d7c7820a2f6140042
Mateusz Stanielewicz <mateusz.stanielewicz@gmail.com>
2015-04-25 12:00:17 +0200

    Created User-Task association

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ActiveRecord::Base
-
   validates :name, presence: true
+
+  belongs_to :author, class_name: 'User'
+  belongs_to :owner, class_name: 'User'
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :authored_tasks, class_name: 'Task', inverse_of: :author, foreign_key: "author_id"
+  has_many :owned_tasks, class_name: 'Task', inverse_of: :owner, foreign_key: "owner_id"
+
 end
